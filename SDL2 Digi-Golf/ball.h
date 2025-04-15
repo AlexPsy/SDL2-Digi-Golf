7c10d6c98c3ae25cafb04a92c99e2851f0f94968
AlexPsy <p.alexey@rocketmail.com>
2025-04-16 00:11:32 +0100

    Refactored as a proper class, as well as moved the controls, dragging checks, and point inside ball check and rendering ball in class.

--- SDL2 Digi-Golf/ball.h
+++ SDL2 Digi-Golf/ball.h
@@ -1,11 +1,39 @@
 #pragma once
+#include <SDL.h>
+#include "constant.h"
 
-struct Ball {
+//struct Ball {
+//	float x, y;
+//	float cx, cy;
+//	bool isMouseDragging;
+//	int dragStartX, dragStartY;
+//	int currentDragX, currentDragY;
+
+//	bool pointInBall(int x, int y) const;
+//};
+
+class Ball {
+public:
+	Ball();
+
+	void Update();
+	void HandleDragEvent(const SDL_Event& event);
+	void Render(SDL_Renderer* renderer, SDL_Texture* texture) const;
+
+	float X() const { return x; }
+	float Y() const { return y; }
+	bool IsDragging() const { return isDragging; }
+	int DragStartX() const { return dragStartX; }
+	int DragStartY() const { return dragStartY; }
+	int CurrentDragX() const { return currentDragX; }
+	int CurrentDragY() const { return currentDragY; }
+
+private:
 	float x, y;
 	float cx, cy;
-	bool isMouseDragging;
+	bool isDragging;
 	int dragStartX, dragStartY;
 	int currentDragX, currentDragY;
 
-	bool pointInBall(int x, int y) const;
+	bool PointInBall(int x, int y) const;
 };
