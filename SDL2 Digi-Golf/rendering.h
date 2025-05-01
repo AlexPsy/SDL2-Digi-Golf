76f5fc41e223338eea4cb61d63d4c0853520c331
AlexPsy <p.alexey@rocketmail.com>
2025-05-01 16:04:18 +0100

    cleaned up rendering.cpp and rendering.h

--- SDL2 Digi-Golf/rendering.h
+++ SDL2 Digi-Golf/rendering.h
@@ -3,8 +3,6 @@
 #include <SDL_image.h>
 #include "ball.h"
 
-//SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path);
-//void renderScene(SDL_Renderer* renderer, const Ball& ball, SDL_Texture* ballTexture);
 
 class Renderer {
 public:
