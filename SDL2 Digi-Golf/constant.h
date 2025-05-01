6439f88ed879aa583ccec03c948497ef83116f6c
AlexPsy <p.alexey@rocketmail.com>
2025-05-01 16:05:14 +0100

    Added data for the level file location (will need changing later when changing levels) as well as tile data

--- SDL2 Digi-Golf/constant.h
+++ SDL2 Digi-Golf/constant.h
@@ -1,8 +1,20 @@
 #pragma once
+#include <string>
 
+// Window size
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
+
+// Ball settings
 const int BALL_RADIUS = 10;
 const float SPEED_FACTOR = 0.10f;
 const float BOUNCE_DAMPING = 0.8f;
+
+// Directional Indicator Settings
 const float LINE_LENGTH = 50.0f;
+
+// Level settings
+const std::string TILE_MAP_PATH = "assets/levels/level1.map";
+const int TILE_SIZE = 32;
+const int COLLISION_TILE = 1;
+const int WALKABLE_TILE = 0;
